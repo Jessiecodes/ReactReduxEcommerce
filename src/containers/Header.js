3beada800e99988dd52dec654c597d99181fcea4
Jessica Leverett <Jessak1202@gmail.com>
2021-07-19 08:00:04 -0500

    first

---
+++ src/containers/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>React & Redux Ecommerce Project</h2> 
+            
+                <button class="ui inverted blue button">
+                        <a target="_blank" href="https://jessicaleverett.com">
+                        Check out my new portfolio site :) 
+                        </a>
+                </button>
+             </div>
+        </div>
+    )
+}
+
+export default Header;
